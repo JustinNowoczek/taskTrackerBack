810a9111cf65c0d9109c9ea3d5f142c562caaf75
TideTwo <thedarkvibe1@gmail.com.com>
2023-07-15 21:02:21 +0200

    s

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,8 +1,12 @@
-const mongoose = require("mongoose");
 require("dotenv").config();
+const mongoose = require("mongoose");
+
 const dbConnectionString = process.env.MONGODB_CONNECTION_STRING;
 
 module.exports = mongoose
   .connect(dbConnectionString)
   .then(() => console.log("Database connected"))
-  .catch((err) => console.log(err));
+  .catch((err) => {
+    console.log("tERROR");
+    return console.log(err);
+  });
